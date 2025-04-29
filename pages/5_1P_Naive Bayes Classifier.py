98a9a453239ef7cc08a715c02a680e529c3e30f8
Wili. A <163724234+Hernimus@users.noreply.github.com>
2025-04-29 20:11:14 +0700

    Update 5_1P_Naive Bayes Classifier.py

--- pages/5_1P_Naive Bayes Classifier.py
+++ pages/5_1P_Naive Bayes Classifier.py
@@ -64,7 +64,7 @@ buttons = [
 # Add the expandable links to the sidebar
 for button_name, section in buttons:
     with st.sidebar.expander(button_name, expanded=False):
-        st.write(f"[{button_name}]({section})", unsafe_allow_html=True)
+        st.markdown(f'<a href="{section}" style="color:white;text-decoration:none;">{button_name}</a>', unsafe_allow_html=True)
 
 
 st.subheader("Dataset")
